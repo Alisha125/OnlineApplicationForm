5ed4706c3f074d447770525b30477ba4417512b7
LENOVO <LENOVO@DESKTOP-HQRG9O9>
2025-03-03 09:00:44 +0530

    Add project files.

---
+++ OnlineApplicationForm/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineApplicationForm.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int Emp_id { get; set; }
+        public string Emp_Name { get; set; }
+    }
+}
